972cf9fe9fc92ad0558e96154d2d3699151a0850
msnimish <94216507+msnimish@users.noreply.github.com>
2022-05-21 16:37:29 +0530

    DOM

---
+++ DOM.js
@@ -0,0 +1,12 @@
+console.log('Welcome to tut12');
+let a= document;
+a= document.all;
+// a=document.body;
+// a= document.forms;
+// Array.from(a).forEach(element => {
+//     console.log(element);
+// });
+
+a = document.links
+console.log(a);
+
